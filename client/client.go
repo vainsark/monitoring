c84d274544e0501cf380637ccd0cca261de7ca8d
vainsark <acid254351@gmail.com>
2025-03-12 22:53:51 +0100

    modified:   client/client.go

--- client/client.go
+++ client/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 	t "time"
 
 	"github.com/shirou/gopsutil/v4/cpu"
@@ -88,7 +87,7 @@ func main() {
 
 		//=======================================================
 
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*t.Second)
 		resp, err := client.LoadData(ctx, metrics)
 		cancel() // cancel the context after the call finishes
 
